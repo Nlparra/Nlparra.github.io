92413fb5d2ae7f11052737d0049ef1fd4a4edd98
Nlparra <53584490+Nlparra@users.noreply.github.com>
2019-11-05 13:18:01 -0500

    fix mobile part and nav header

---
+++ main.js
@@ -0,0 +1,6 @@
+let navBar = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click',function name() {
+    mainNav.classList.toggle('thisishow');
+});
